adc990252be861011f06d2c523745afa0438218c
alan <alanarostegui@hotmail.com>
2020-07-08 21:31:06 -0500

    variables

---
+++ bin/variables.dart
@@ -0,0 +1,24 @@
+void main(List<String> arguments) {
+  print('Hello world!');
+  int valInt = 25;
+  double valDouble = 1.0;
+  String valString = 'Hola';
+  bool valBool = true;
+
+  print('$valString mi nombre es Alan');
+  print('$valString mi nombre es Alan' + " y concateno mi edad $valInt años");
+  print('''$valString estoy haciendolo con saltos largos
+  
+  
+      aqui hice un salto
+  ''');
+
+  print(r"q323435%&&    \z");
+
+  print(valString);
+  print(valBool);
+  print(valDouble);
+  print(valInt);
+
+  print(valString.isEmpty);
+}
