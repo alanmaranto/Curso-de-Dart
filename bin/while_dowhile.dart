776f9327a460b0aa75a740a8ecf20dea3de844d0
alan <alanarostegui@hotmail.com>
2020-07-23 18:55:09 -0500

    while y do while

---
+++ bin/while_dowhile.dart
@@ -0,0 +1,13 @@
+main(List<String> args) {
+  int i = 0;
+  while (i <= 10) {
+    print('Valor actual $i');
+    i++;
+  }
+
+  i = 1;
+  do {
+    print('Valor actual $i');
+    i++;
+  } while (i <= 10);
+}
